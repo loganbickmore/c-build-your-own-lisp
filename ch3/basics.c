b3eca62bcdfc7c3d78314070787c49d6d0c776a3
Logan Bickmore <loganbickmore91@gmail.com>
2018-03-01 20:53:16 -0800

    added .on-save.json autoformatting for c

--- ch3/basics.c
+++ ch3/basics.c
@@ -9,37 +9,36 @@ int add_together(int x, int y) {
 // struct declaration ex
 typedef struct {
   float x;
-  floaty;
+  float y;
 } point;
 
-
-int hello_n(int n, char* str) {
+int hello_n(int n, char *str) {
   for (int i = 0; i < n; i++) {
     printf("func hello_n iteration %d - %s\n", i + 1, str);
   }
   return 0;
 }
 
-int loops(char* str) {
+int loops(char *str) {
   for (int i = 0; i < 5; i++) {
     printf("for iteration %d - %s\n", i + 1, str);
   }
 
   int i = 0;
-  while ( i < 5 ) {
+  while (i < 5) {
     printf("while iteration %d - %s\n", i + 1, str);
     i++;
   }
-  
+
   return 0;
 }
 
-int main(int argc, char** argv) {
-  char* str = "Hello World!";
+int main(int argc, char **argv) {
+  char *str = "Hello World!";
 
   loops(str);
 
-  hello_n(2,str);
+  hello_n(2, str);
 
   return 0;
 }
