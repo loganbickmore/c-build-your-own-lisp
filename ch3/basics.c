37273435c31371c932e093aa7c5fcda7f3f83e49
Logan Bickmore <loganbickmore91@gmail.com>
2018-03-01 20:46:08 -0800

    update

--- ch3/basics.c
+++ ch3/basics.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+// function declaration ex
+int add_together(int x, int y) {
+  int result = x + y;
+  return result;
+}
+
+// struct declaration ex
+typedef struct {
+  float x;
+  floaty;
+} point;
+
+
 int hello_n(int n, char* str) {
   for (int i = 0; i < n; i++) {
     printf("func hello_n iteration %d - %s\n", i + 1, str);
@@ -7,9 +20,7 @@ int hello_n(int n, char* str) {
   return 0;
 }
 
-int main(int argc, char** argv) {
-  char* str = "Hello World!";
-
+int loops(char* str) {
   for (int i = 0; i < 5; i++) {
     printf("for iteration %d - %s\n", i + 1, str);
   }
@@ -19,6 +30,14 @@ int main(int argc, char** argv) {
     printf("while iteration %d - %s\n", i + 1, str);
     i++;
   }
+  
+  return 0;
+}
+
+int main(int argc, char** argv) {
+  char* str = "Hello World!";
+
+  loops(str);
 
   hello_n(2,str);
 
