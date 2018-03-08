be74921754696b48eeb0ac041278577e895827b5
Logan Bickmore <loganbickmore91@gmail.com>
2018-03-07 22:35:58 -0800

    started chapter 7

---
+++ ch7/parsing.h
@@ -0,0 +1,20 @@
+// windows preprocessor
+#ifdef _WIN32
+
+static char buffer[2048];
+
+char *readline(char *prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char *cpy = malloc(strlen(buffer) + 1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy) - 1] = '\0';
+  return cpy;
+}
+
+void add_history(char *unused) {}
+
+#else
+#include <editline/history.h>
+#include <editline/readline.h>
+#endif
